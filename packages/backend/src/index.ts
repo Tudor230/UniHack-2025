b52f2a8102d3c639593d744ee19681f17ec179f0
Paul Soporan <paul.soporan@gmail.com>
2025-11-16 02:25:33 +0200

    chore: bind port

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -111,7 +111,7 @@ function buildApp() {
 
 const fastify = buildApp();
 
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port: process.env.PORT || 3000 }, function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
