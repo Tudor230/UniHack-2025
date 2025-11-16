c471df3ca7904450166881edeba9c0b78a593b5b
Paul Soporan <paul.soporan@gmail.com>
2025-11-16 02:30:07 +0200

    chore: bind host

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -111,7 +111,7 @@ function buildApp() {
 
 const fastify = buildApp();
 
-fastify.listen({ port: process.env.PORT || 3000 }, function (err, address) {
+fastify.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" }, function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
